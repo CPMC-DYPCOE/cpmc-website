09a1363ffe806c7fa7f67b12d6a3b53b26c7f45f
Spider9480 <omgawandeofficial9834899149@gmail.com>
2022-06-03 13:31:11 +0530

    document file added

---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Html, Main, Head, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="preload"
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600&family=Paytone+One&display=swap"
+            as="font"
+            crossOrigin="anonymous"
+          ></link>
+        </Head>
+        <body>
+          <Main></Main>
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
