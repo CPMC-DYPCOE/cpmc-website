b06a709ac0731f4e672352c7a450eef14233b733
Spider9480 <omgawandeofficial9834899149@gmail.com>
2022-06-08 20:39:20 +0530

    admin page styling fixed

--- components/AdminPage/AdminPage.jsx
+++ components/AdminPage/AdminPage.jsx
@@ -23,65 +23,70 @@ const AdminPage = () => {
   }, []);
 
   return (
-    <div className={classes.container}>
-      <h1 style={{ fontSize: '2rem' }}>Admin Page</h1>
-      <hr />
-      <h1 style={{ fontSize: '2rem' }}>Contacts</h1>
-      <div className={classes.contactContainer}>
-        {contacts.map((contact) => {
-          return (
-            <div key={contact._id} className={classes.contact}>
-              <h1>Name: {contact.name}</h1>
-              <h3>Email: {contact.email}</h3>
-              <p>Message: {contact.message}</p>
-            </div>
-          );
-        })}
+    <>
+      <h1 className={classes.admin}>Admin</h1>
+      <div className={classes.outer_container}>
+        <div className={classes.inner_block}>
+          <h1 className={classes.heading}>
+            Contact<span className={classes.red}> Us</span>
+          </h1>
+          <div className={classes.data_items}>
+            {contacts.map((contact) => {
+              return (
+                <div key={contact._id} className={classes.data_item}>
+                  <h1>Name: {contact.name}</h1>
+                  <h3>Email: {contact.email}</h3>
+                  <p>Message: {contact.message}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className={classes.inner_block}>
+          <h1 className={classes.heading}>
+            Students Interested to<span className={classes.red}> Join Club</span>
+          </h1>
+          <div className={classes.data_item2}>
+            {students.map((student) => {
+              const {
+                _id,
+                firstName,
+                branch,
+                email,
+                phoneNumber,
+                academicYear,
+                division,
+                leetCodeId,
+                codeChefId,
+                codeForcesId,
+                atCoderId,
+                hackerRankId,
+                hackerEarthId
+              } = student;
+              return (
+                <div key={_id} className={classes.data_items2}>
+                  <h1>Name: {firstName}</h1>
+                  <h3>Branch: {branch}</h3>
+                  <h3>Email: {email}</h3>
+                  <p>Phone Number: {phoneNumber}</p>
+                  <p>Academic Year: {academicYear}</p>
+                  <p>Division: {division}</p>
+                  {leetCodeId && <a href={leetCodeId}>LeetCode</a>}
+                  {codeChefId && <a href={codeChefId}>CodeChef</a>}
+                  {codeForcesId && <a href={codeForcesId}>CodeForces</a>}
+                  {atCoderId && <a href={atCoderId}>AtCoder</a>}
+                  {hackerRankId && <a href={hackerRankId}>HackerRank</a>}
+                  {hackerEarthId && <a href={hackerEarthId}>HackerEarth</a>}
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
-
-      <h1 style={{ fontSize: '2rem' }}>Students Interested to Join club</h1>
-      <div className={classes.contactContainer}>
-        {students.map((student) => {
-          const {
-            _id,
-            firstName,
-            branch,
-            email,
-            phoneNumber,
-            academicYear,
-            division,
-            leetCodeId,
-            codeChefId,
-            codeForcesId,
-            atCoderId,
-            hackerRankId,
-            hackerEarthId
-          } = student;
-          return (
-            <div key={_id} className={classes.contact}>
-              <h1>Name: {firstName}</h1>
-              <h3>Branch: {branch}</h3>
-              <h3>Email: {email}</h3>
-              <p>Phone Number: {phoneNumber}</p>
-              <p>Academic Year: {academicYear}</p>
-              <p>Division: {division}</p>
-              {leetCodeId && <a href={leetCodeId}>LeetCode</a>}
-              {codeChefId && <a href={codeChefId}>CodeChef</a>}
-              {codeForcesId && <a href={codeForcesId}>CodeForces</a>}
-              {atCoderId && <a href={atCoderId}>AtCoder</a>}
-              {hackerRankId && <a href={hackerRankId}>HackerRank</a>}
-              {hackerEarthId && <a href={hackerEarthId}>HackerEarth</a>}
-            </div>
-          );
-        })}
-      </div>
-      <div className={classes.xyz}>
-        {students.map((student, key) => {
-          return <h1 key={key}>{student.email}</h1>;
-        })}
-      </div>
-    </div>
+    </>
   );
 };
 
 export default AdminPage;
+
+//Yoo
