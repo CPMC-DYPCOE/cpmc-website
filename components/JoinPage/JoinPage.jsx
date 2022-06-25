01ab4ebc5adefee041ed5960ec19848cd08ac166
Spider9480 <omgawandeofficial9834899149@gmail.com>
2022-06-25 18:29:31 +0530

    registrations closed

--- components/JoinPage/JoinPage.jsx
+++ components/JoinPage/JoinPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import classes from './JoinPage.module.css';
 import Link from 'next/link';
 import { ToastContainer } from 'react-toastify';
@@ -7,6 +7,8 @@ import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 
 const JoinPage = () => {
+  const [registraion, setRegistraion] = useState(true);
+
   const nameInput = useRef();
   const emailInput = useRef();
   const phoneNumberInput = useRef();
@@ -75,135 +77,162 @@ const JoinPage = () => {
     <>
       <div className={classes.join_section}>
         <ToastContainer />
-        <h1 className={classes.heading}>
-          FILL THESE DETAILS TO <br /> <span className={classes.red}> JOIN CLUB</span>
-        </h1>
-        <p className={classes.para}>
-          Fill out the form below to join the club and become a member of the C.P.M.C squad.
-        </p>
-        <div className={classes.form}>
-          <div className={classes.input_container}>
-            <label className={classes.input_label}>
-              First Name<span className={classes.mandatory}>*</span>
-            </label>
-            <input className={classes.input} placeholder="Rahul Sharma" ref={nameInput} />
-          </div>
-          <div className={classes.input_container}>
-            <label className={classes.input_label}>
-              Email<span className={classes.mandatory}>*</span>
-            </label>
-            <input className={classes.input} placeholder="rahulsharma@gmail.com" ref={emailInput} />
-          </div>
-          <div className={classes.input_container}>
-            <label className={classes.input_label}>
-              Phone Number<span className={classes.mandatory}>*</span>
-            </label>
-            <input className={classes.input} placeholder="0123456789" ref={phoneNumberInput} />
-          </div>
-          <div className={classes.input_container}>
-            <label className={classes.input_label}>
-              Academic Year<span className={classes.mandatory}>*</span>
-            </label>
-            <select className={classes.input} ref={academicYearInput}>
-              <option className={classes.option}>Choose your year</option>
-              <option className={classes.option}>FE</option>
-              <option className={classes.option}>SE</option>
-              <option className={classes.option}>TE</option>
-              <option className={classes.option}>BE</option>
-            </select>
-          </div>
-          <div className={classes.input_container}>
-            <label className={classes.input_label}>
-              Branch<span className={classes.mandatory}>*</span>
-            </label>
-            <select className={classes.input} ref={branchInput}>
-              <option>Choose your branch</option>
-              <option>Computer Engineering</option>
-              <option>IT Engineering</option>
-              <option>AI&DS Engineering</option>
-              <option>Mechanical Engineering</option>
-              <option>Robotics and Automation</option>
-              <option>ENTC Engineering</option>
-              <option>Civil Engineering</option>
-              <option>Instrumental Engineering</option>
-            </select>
-          </div>
-          <div className={classes.input_container}>
-            <label className={classes.input_label}>
-              Division<span className={classes.mandatory}>*</span>
-            </label>
-            <select className={classes.input} ref={divisionInput}>
-              <option>Choose your division</option>
-              <option>A</option>
-              <option>B</option>
-            </select>
-          </div>
-          <div className={classes.input_container}>
-            <label className={classes.input_label}>
-              Hackerrank ID<span className={classes.mandatory}>*</span>
-            </label>
-            <input
-              className={classes.input}
-              placeholder="https://www.hackerrank.com/rahul_sharma"
-              ref={hackerRankIdInput}
-            />
-          </div>
-          <div className={classes.input_container}>
-            <label className={classes.input_label}>Codechef ID</label>
-            <input
-              className={classes.input}
-              placeholder="https://www.codechef.com/users/rahul_sharma"
-              ref={codeChefIdInput}
-            />
-          </div>
-          <div className={classes.input_container}>
-            <label className={classes.input_label}>Leetcode ID</label>
-            <input
-              className={classes.input}
-              placeholder="https://leetcode.com/rahul_sharma/"
-              ref={leetCodeIdInput}
-            />
-          </div>
-          <div className={classes.input_container}>
-            <label className={classes.input_label}>Codeforces ID</label>
-            <input
-              className={classes.input}
-              placeholder="https://codeforces.com/profile/rahul_sharma"
-              ref={codeForcesIdInput}
-            />
-          </div>
-          <div className={classes.input_container}>
-            <label className={classes.input_label}>Atcoder ID</label>
-            <input
-              className={classes.input}
-              placeholder="https://atcoder.jp/users/rahul_sharma"
-              ref={atCoderIdInput}
-            />
-          </div>
-          <div className={classes.input_container}>
-            <label className={classes.input_label}>Hackerearth ID</label>
-            <input
-              className={classes.input}
-              placeholder="https://www.hackerearth.com/@rahul_sharrma"
-              ref={hackerEarthIdInput}
-            />
-          </div>
+        {registraion ? (
+          <>
+            <h1 className={classes.heading}>
+              Registrations <br /> <span className={classes.red}> Closed</span>
+            </h1>
+            <p className={classes.para}>Currently registrations are closed</p>
+          </>
+        ) : (
+          <>
+            <h1 className={classes.heading}>
+              FILL THESE DETAILS TO <br /> <span className={classes.red}> JOIN CLUB</span>
+            </h1>
+            <p className={classes.para}>
+              Fill out the form below to join the club and become a member of the C.P.M.C squad.
+            </p>
+          </>
+        )}
 
-          <button className={classes.btn} onClick={submitFormHandler}>
-            <div className={classes['svg-wrapper-1']}>
-              <div className={classes['svg-wrapper']}>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                  <path fill="none" d="M0 0h24v24H0z"></path>
-                  <path
-                    fill="currentColor"
-                    d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
-                  ></path>
-                </svg>
+        {!registraion ? (
+          <>
+            <div className={classes.form}>
+              <div className={classes.input_container}>
+                <label className={classes.input_label}>
+                  First Name<span className={classes.mandatory}>*</span>
+                </label>
+                <input className={classes.input} placeholder="Rahul Sharma" ref={nameInput} />
               </div>
+              <div className={classes.input_container}>
+                <label className={classes.input_label}>
+                  Email<span className={classes.mandatory}>*</span>
+                </label>
+                <input
+                  className={classes.input}
+                  placeholder="rahulsharma@gmail.com"
+                  ref={emailInput}
+                />
+              </div>
+              <div className={classes.input_container}>
+                <label className={classes.input_label}>
+                  Phone Number<span className={classes.mandatory}>*</span>
+                </label>
+                <input className={classes.input} placeholder="0123456789" ref={phoneNumberInput} />
+              </div>
+              <div className={classes.input_container}>
+                <label className={classes.input_label}>
+                  Academic Year<span className={classes.mandatory}>*</span>
+                </label>
+                <select className={classes.input} ref={academicYearInput}>
+                  <option className={classes.option}>Choose your year</option>
+                  <option className={classes.option}>FE</option>
+                  <option className={classes.option}>SE</option>
+                  <option className={classes.option}>TE</option>
+                  <option className={classes.option}>BE</option>
+                </select>
+              </div>
+              <div className={classes.input_container}>
+                <label className={classes.input_label}>
+                  Branch<span className={classes.mandatory}>*</span>
+                </label>
+                <select className={classes.input} ref={branchInput}>
+                  <option>Choose your branch</option>
+                  <option>Computer Engineering</option>
+                  <option>IT Engineering</option>
+                  <option>AI&DS Engineering</option>
+                  <option>Mechanical Engineering</option>
+                  <option>Robotics and Automation</option>
+                  <option>ENTC Engineering</option>
+                  <option>Civil Engineering</option>
+                  <option>Instrumental Engineering</option>
+                </select>
+              </div>
+              <div className={classes.input_container}>
+                <label className={classes.input_label}>
+                  Division<span className={classes.mandatory}>*</span>
+                </label>
+                <select className={classes.input} ref={divisionInput}>
+                  <option>Choose your division</option>
+                  <option>A</option>
+                  <option>B</option>
+                </select>
+              </div>
+              <div className={classes.input_container}>
+                <label className={classes.input_label}>
+                  Hackerrank ID<span className={classes.mandatory}>*</span>
+                </label>
+                <input
+                  className={classes.input}
+                  placeholder="https://www.hackerrank.com/rahul_sharma"
+                  ref={hackerRankIdInput}
+                />
+              </div>
+              <div className={classes.input_container}>
+                <label className={classes.input_label}>Codechef ID</label>
+                <input
+                  className={classes.input}
+                  placeholder="https://www.codechef.com/users/rahul_sharma"
+                  ref={codeChefIdInput}
+                />
+              </div>
+              <div className={classes.input_container}>
+                <label className={classes.input_label}>Leetcode ID</label>
+                <input
+                  className={classes.input}
+                  placeholder="https://leetcode.com/rahul_sharma/"
+                  ref={leetCodeIdInput}
+                />
+              </div>
+              <div className={classes.input_container}>
+                <label className={classes.input_label}>Codeforces ID</label>
+                <input
+                  className={classes.input}
+                  placeholder="https://codeforces.com/profile/rahul_sharma"
+                  ref={codeForcesIdInput}
+                />
+              </div>
+              <div className={classes.input_container}>
+                <label className={classes.input_label}>Atcoder ID</label>
+                <input
+                  className={classes.input}
+                  placeholder="https://atcoder.jp/users/rahul_sharma"
+                  ref={atCoderIdInput}
+                />
+              </div>
+              <div className={classes.input_container}>
+                <label className={classes.input_label}>Hackerearth ID</label>
+                <input
+                  className={classes.input}
+                  placeholder="https://www.hackerearth.com/@rahul_sharrma"
+                  ref={hackerEarthIdInput}
+                />
+              </div>
+
+              <button className={classes.btn} onClick={submitFormHandler}>
+                <div className={classes['svg-wrapper-1']}>
+                  <div className={classes['svg-wrapper']}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      width="24"
+                      height="24"
+                    >
+                      <path fill="none" d="M0 0h24v24H0z"></path>
+                      <path
+                        fill="currentColor"
+                        d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
+                      ></path>
+                    </svg>
+                  </div>
+                </div>
+                <span>SUBMIT</span>
+              </button>
             </div>
-            <span>SUBMIT</span>
-          </button>
-        </div>
+          </>
+        ) : (
+          <></>
+        )}
       </div>
     </>
   );
