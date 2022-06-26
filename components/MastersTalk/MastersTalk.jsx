7194210f81e48ddd10a9d11818bdc3269bc50685
Sarthak Pawar <sarthakrajesh777@gmail.com>
2022-06-26 12:38:42 +0530

    whatsapp button

--- components/MastersTalk/MastersTalk.jsx
+++ components/MastersTalk/MastersTalk.jsx
@@ -20,7 +20,7 @@ const MasterTalk = () => {
   const ieltsInput = useRef();
   const duolingoInput = useRef();
   const aimInput = useRef();
-  const [registered, setRegistered] = useState(false)
+  const [registered, setRegistered] = useState(true)
   const [visit, setVisit] = useState(0)
 
   const linkMap = ['https://chat.whatsapp.com/KAXnEQpSSYe2L1hOltO1it', 'https://chat.whatsapp.com/LIOZnHu3iXf5GGnPzw5epu']
@@ -228,10 +228,10 @@ const MasterTalk = () => {
       <h1 className={classes.input_label} style={{ textAlign: 'center', fontWeight: 'bold' }}>
         Registered Successfully!!!
       </h1>
-      <p style={{ textAlign: 'center' }}>Join our WhatsApp group for further updates</p>
+      <p style={{ textAlign: 'center' }}>Join any one of our WhatsApp group for further updates</p>
       <br />
       <a href={linkMap[1]} style={{ backgroundColor: '#ff2863' }} onClick={buttonToggle} className={classes.linkBtn}>WhatsApp Group 1</a>
-      {/* {visit === true ? () : (<a href={linkMap[0]} style={{ backgroundColor: '#4073ff' }} onClick={buttonToggle} className={classes.linkBtn}>WhatsApp Group 2</a>)} */}
+      <a href={linkMap[0]} style={{ backgroundColor: '#4073ff' }} onClick={buttonToggle} className={classes.linkBtn}>WhatsApp Group 2</a>
     </div>
     )
   }
