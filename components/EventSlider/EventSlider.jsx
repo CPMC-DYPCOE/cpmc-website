3dc5a0a9c3c0ec62ebc551f1b9ba29d7da3915da
Spider9480 <omgawandeofficial9834899149@gmail.com>
2022-06-26 10:01:01 +0530

    fixed

--- components/EventSlider/EventSlider.jsx
+++ components/EventSlider/EventSlider.jsx
@@ -10,7 +10,7 @@ import 'swiper/css/pagination';
 import 'swiper/css/zoom';
 
 import abcde from './Images/registration.png';
-import classes from './eventSlider.module.css'
+import classes from './eventSlider.module.css';
 import 'swiper/css/effect-cards';
 import { Zoom, Autoplay, Navigation, Pagination } from 'swiper';
 
@@ -18,26 +18,32 @@ const EventSlider = () => {
   return (
     <>
       <div className="pt-20 lg:pl-32 lg:pr-32">
-        <h1 className="ml-12 text-center font-Paytone text-xl font-semibold text-white lg:ml-0 lg:text-4xl" >
+        <h1 className="ml-12 text-center font-Paytone text-xl font-semibold text-white lg:ml-0 lg:text-4xl">
           Upcoming <span className="text-[#f32053]"> Events </span>{' '}
         </h1>
       </div>
       <div className={classes.eventmaindiv}>
         <div className={classes.imagediv}>
-        <Image alt="eventabcde" src={abcde} objectFit="cover" className={classes.posterimage} />
+          <Image alt="eventabcde" src={abcde} objectFit="cover" className={classes.posterimage} />
         </div>
         <div className={classes.eventcontentdiv}>
-          <h1>MASTERS' <span>TALK</span></h1>
-          <h3>A Guide for further studies from seniors who have secured themselves a seat in top universities all over the world and India. he talk will be about They will guide students about how to start preparation</h3>
+          <h1>
+            MASTERS&apos; <span>TALK</span>
+          </h1>
+          <h3>
+            A Guide for further studies from seniors who have secured themselves a seat in top
+            universities all over the world and India. he talk will be about They will guide
+            students about how to start preparation
+          </h3>
           <Link href="events">
-              <button className={classes.btn}>
-                <span className={classes.span}>Register Now</span>
-                <svg viewBox="0 0 13 10" height="10px" width="15px">
-                  <path d="M1,5 L11,5"></path>
-                  <polyline points="8 1 12 5 8 9"></polyline>
-                </svg>
-              </button>
-            </Link>
+            <button className={classes.btn}>
+              <span className={classes.span}>Register Now</span>
+              <svg viewBox="0 0 13 10" height="10px" width="15px">
+                <path d="M1,5 L11,5"></path>
+                <polyline points="8 1 12 5 8 9"></polyline>
+              </svg>
+            </button>
+          </Link>
         </div>
       </div>
       {/* <div className="m-12 lg:m-[300px] lg:mt-12 lg:mb-0 lg:pl-16 lg:pr-16">
