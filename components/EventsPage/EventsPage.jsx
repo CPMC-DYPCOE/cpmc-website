c9ccd483533f3c2b2a4b3681e5687d26e794d05d
Aryan Jangid <aryanjangid2511@gmail.com>
2022-11-01 22:59:53 +0530

    event page updated

--- components/EventsPage/EventsPage.jsx
+++ components/EventsPage/EventsPage.jsx
@@ -5,104 +5,36 @@ import e1 from '../assets/Event1.png';
 import e2 from '../assets/mastertalk.png';
 import Image from 'next/image';
 import Link from 'next/link';
+import UpcomingEventCard from '../UpcomingEventCard/UpcomingEventCard';
+import PastEventCard from '../PastEventCard/PastEventCard';
 
 const EventsPage = () => {
+
+  const upcomingEvent = [
+    { name: 'Code Room', subTitle: 'Come code together learn together', toBeHeldOn: 'Monday to Friday', timing: '5pm', Venue: 'Classroom Number 1, A building', registrationLink: '', img: e1 }
+  ]
+
+  const pastEvent = [
+    { name: 'Masters', subTitle: 'A talk with the Masters for Masters', heldOn: '4/July/2022', timing: '', Venue: 'sfd', ViewEventDetailsLink: '' },
+    { name: 'Coding Competition', subTitle: 'Programming Competition, for students and professionals of DYPCOE Akurdi, Pune', heldOn: '5/June/2022', timing: '', Venue: 'Online', ViewEventDetailsLink: ''},
+  ]
+
   return (
     <>
       <div className={styles.event_section}>
-        <h1 className={styles.title}>
-          Upcoming <span className={styles.red}> Events</span>
-        </h1>
-        <div className={styles.event}>
-          <div className={styles.blue_box}></div>
-          <div className={styles.content_container}>
-            <div className={styles.content}>
-              <h1 className={styles.name}>
-                Code <span className={styles.red}>Room</span>
-              </h1>
-              <p className={styles.desc}>A talk with the Masters for Masters</p>
-              <p className={styles.date}>
-                DATE - <span className={styles.blue}>Not Defined</span>
-              </p>
-              <span className={styles.link}>
-                <Link href="/masterstalk">
-                  <button className={classes.btn} style={{ margin: '0' }}>
-                    <span className={classes.span}>REGISTER HERE!!</span>
-                    <svg viewBox="0 0 13 10" height="10px" width="15px">
-                      <path d="M1,5 L11,5"></path>
-                      <polyline points="8 1 12 5 8 9"></polyline>
-                    </svg>
-                  </button>
-                </Link>
-              </span>
-            </div>
-            <div className={styles.xyz}>
-              <Image alt="xyz" src={e2} width={270} height={250} />
-            </div>
-          </div>
-        </div>
+        <h1 className={styles.title}> Upcoming <span className={styles.red}> Events</span></h1>
+        {upcomingEvent.map((data) => {
+          return (<UpcomingEventCard data={data} />)
+        })
+        }
       </div>
       <div className={styles.event_section}>
-        <h1 className={styles.title}>
-          Past <span className={styles.red}> Events</span>
-        </h1>
-        <div className={styles.event}>
-          <div className={styles.blue_box}></div>
-          <div className={styles.content_container}>
-            <div className={styles.content}>
-              <h1 className={styles.name}>
-                Masters&apos; <span className={styles.red}>Talk</span>
-              </h1>
-              <p className={styles.desc}>A talk with the Masters for Masters</p>
-              <p className={styles.date}>
-                Held On - <span className={styles.blue}>4/July/2022</span>
-              </p>
-              <span className={styles.link}>
-                <Link href="/masterstalk">
-                  <button className={classes.btn} style={{ margin: '0' }}>
-                    <span className={classes.span}>VIEW EVENT</span>
-                    <svg viewBox="0 0 13 10" height="10px" width="15px">
-                      <path d="M1,5 L11,5"></path>
-                      <polyline points="8 1 12 5 8 9"></polyline>
-                    </svg>
-                  </button>
-                </Link>
-              </span>
-            </div>
-            <div className={styles.xyz}>
-              <Image alt="xyz" src={e2} width={270} height={250} />
-            </div>
-          </div>
-        </div>
-        <div className={styles.event}>
-          <div className={styles.blue_box}></div>
-          <div className={styles.content_container}>
-            <div className={styles.content}>
-              <h1 className={styles.name}>
-                CODING <span className={styles.red}>COMPETION</span>
-              </h1>
-              <p className={styles.desc}>
-                Programming Competition, for students and professionals of DYPCOE Akurdi, Pune.
-              </p>
-              <p className={styles.date}>
-                Held On - <span className={styles.blue}>5/June/2022</span>
-              </p>
-              <span className={styles.link}>
-                <Link href="/masterstalk">
-                  <button className={classes.btn} style={{ margin: '0' }}>
-                    <span className={classes.span}>VIEW EVENT</span>
-                    <svg viewBox="0 0 13 10" height="10px" width="15px">
-                      <path d="M1,5 L11,5"></path>
-                      <polyline points="8 1 12 5 8 9"></polyline>
-                    </svg>
-                  </button>
-                </Link>
-              </span>
-            </div>
-            <div className={styles.xyz}>
-              <Image alt="xyz" src={e1} width={250} height={200} />
-            </div>
-          </div>
+        <h1 className={styles.title}>Past <span className={styles.red}> Events</span></h1>
+        <div className={styles.event_content_section}>
+          {pastEvent.map((data) => {
+            return (<PastEventCard data={data} />)
+          })
+          }
         </div>
       </div>
     </>
---
+++ components/PastEventCard/PastEventCard.jsx
@@ -0,0 +1,40 @@
+import styles from './PastEventCard.module.css'
+import Link from 'next/link';
+import classes from '../LandingSlide/LandingSlide.module.css';
+import Image from 'next/image';
+
+const PastEventCard = (props) => {
+    const name = props.data.name;
+    const nameArr = name.split(' ');
+    return (
+        <div className={styles.event}>
+            <div className={styles.blue_box}></div>
+            <div className={styles.content_container}>
+                <div className={styles.content}>
+                    {nameArr.length > 1 ?
+                        <h1 className={styles.name}>{nameArr[0]} <span className={styles.red}>{nameArr[1]}</span></h1> :
+                        <h1 className={styles.name}> <span className={styles.red}>{props.data.name}</span></h1>
+                    }
+                    {props.data.subTitle ? <p className={styles.desc}>{props.data.subTitle}</p> : null}
+                    {props.data.heldOn ? <p className={styles.date}> Held On - <span className={styles.blue}>{props.data.heldOn}</span> </p> : null}
+                    {props.data.ViewEventDetailsLink?
+                    <span className={styles.link}>
+                        <Link href={props.data.ViewEventDetailsLink}>
+                            <button className={classes.btn} style={{ margin: '0' }}>
+                                <span className={classes.span}>VIEW EVENT</span>
+                                <svg viewBox="0 0 13 10" height="10px" width="15px">
+                                    <path d="M1,5 L11,5"></path>
+                                    <polyline points="8 1 12 5 8 9"></polyline>
+                                </svg>
+                            </button>
+                        </Link>
+                    </span>:
+                    null
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PastEventCard
