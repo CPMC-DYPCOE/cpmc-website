3aecdf55d0f308a60d4d58fd81a9d53ac8fab9ab
Aryan Jangid <aryanjangid2511@gmail.com>
2023-09-18 01:03:06 +0530

    loading added

--- components/EventDetails/EventDetails.jsx
+++ components/EventDetails/EventDetails.jsx
@@ -5,7 +5,7 @@ import { API_HOST } from '../../utils/utils';
 import Image from 'next/image';
 
 const EventDetails = ({ event_id }) => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const [eventDetails, setEventDetails] = useState({
     event_id: '',
@@ -33,14 +33,14 @@ const EventDetails = ({ event_id }) => {
           return res.json();
         })
         .then((data) => {
-          console.log(data.event_details[0]);
           setEventDetails(data.event_details[0]);
+          setIsLoading(false);
         });
     } catch (error) {
       console.log(error);
       alert('Something Went Wrong');
+      setIsLoading(false);
     }
-    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -51,49 +51,55 @@ const EventDetails = ({ event_id }) => {
   }, [event_id]);
 
   return (
-    <div className={classes.eventDetails}>
-      <div className={classes.generalInfo}>
-        <h1 className={classes.heading}>{eventDetails.event_name}</h1>
-        <h1 className={classes.caption}>{eventDetails.caption}</h1>
-        <h1 className={classes.generalDetails}>
-          <span>Date: </span>
-          {eventDetails.event_date}
-        </h1>
-        <h1 className={classes.generalDetails}>
-          <span>Time: </span>
-          {eventDetails.event_time}
-        </h1>
-        <h1 className={classes.generalDetails}>
-          <span>Venue: </span>
-          {eventDetails.venue}
-        </h1>
-      </div>
-      <div className={classes.aboutEvent}>
-        <h1>About Event: </h1>
-        <p style={{maxWidth:"500px", textAlign:"center"}}> {eventDetails.description}</p>
-      </div>
-      <div>
-        {!eventDetails.is_completed ? (
-          <Link href={{ pathname: `/register/${event_id}` }}>
-            <button className={classes.btn} style={{ margin: '0' }}>
-              <span className={classes.span}>Register here</span>
-              <svg viewBox="0 0 13 10" height="10px" width="15px">
-                <path d="M1,5 L11,5"></path>
-              </svg>
-            </button>
-          </Link>
-        ) : null}
-      </div>
-      {eventDetails.images.length > 0 ? (
-        <div className={classes.eventImages}>
-          <h1 className={classes.heading}>Memories</h1>
-          <div className={classes.allImages}> 
-            {eventDetails.images.map((src,id) => {
-              return <img src={src} key={id}/>;
-            })}
+    <div>
+      {isLoading == true ? (
+        <div style={{ color: 'white', margin: '10em 2em', textAlign: 'center' }}>Loading...</div>
+      ) : (
+        <div className={classes.eventDetails}>
+          <div className={classes.generalInfo}>
+            <h1 className={classes.heading}>{eventDetails.event_name}</h1>
+            <h1 className={classes.caption}>{eventDetails.caption}</h1>
+            <h1 className={classes.generalDetails}>
+              <span>Date: </span>
+              {eventDetails.event_date}
+            </h1>
+            <h1 className={classes.generalDetails}>
+              <span>Time: </span>
+              {eventDetails.event_time}
+            </h1>
+            <h1 className={classes.generalDetails}>
+              <span>Venue: </span>
+              {eventDetails.venue}
+            </h1>
           </div>
+          <div className={classes.aboutEvent}>
+            <h1>About Event: </h1>
+            <p style={{ maxWidth: '500px', textAlign: 'center' }}> {eventDetails.description}</p>
+          </div>
+          <div>
+            {!eventDetails.is_completed ? (
+              <Link href={{ pathname: `/register/${event_id}` }}>
+                <button className={classes.btn} style={{ margin: '0' }}>
+                  <span className={classes.span}>Register here</span>
+                  <svg viewBox="0 0 13 10" height="10px" width="15px">
+                    <path d="M1,5 L11,5"></path>
+                  </svg>
+                </button>
+              </Link>
+            ) : null}
+          </div>
+          {eventDetails.images.length > 0 ? (
+            <div className={classes.eventImages}>
+              <h1 className={classes.heading}>Memories</h1>
+              <div className={classes.allImages}>
+                {eventDetails.images.map((src, id) => {
+                  return <img src={src} key={id} />;
+                })}
+              </div>
+            </div>
+          ) : null}
         </div>
-      ) : null}
+      )}
     </div>
   );
 };
--- components/EventsPage/EventsPage.jsx
+++ components/EventsPage/EventsPage.jsx
@@ -7,7 +7,7 @@ import { API_HOST } from '../../utils/utils';
 const EventsPage = () => {
   const [upComingEvent, setUpcomingEvent] = useState([]);
   const [pastEvent, setPastEvent] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const getEvents = async () => {
     try {
@@ -21,12 +21,13 @@ const EventsPage = () => {
           const filteredPastEvents = events.filter((event) => event.is_completed);
           setUpcomingEvent(filteredUpcomingEvents);
           setPastEvent(filteredPastEvents);
+          setIsLoading(false);
         });
     } catch (error) {
       console.log(error);
       alert('Something Went Wrong');
+      setIsLoading(false);
     }
-    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -36,29 +37,35 @@ const EventsPage = () => {
 
   return (
     <>
-      {upComingEvent.length > 0 ? (
-        <div className={styles.event_section}>
-          <h1 className={styles.title}>
-            {' '}
-            Upcoming <span className={styles.red}> Events</span>
-          </h1>
-          {upComingEvent.map((data, i) => {
-            return <UpcomingEventCard data={data} key={i} />;
-          })}
+      {isLoading == true ? (
+        <div style={{ color: 'white', margin: '10em 2em', textAlign: 'center' }}>Loading...</div>
+      ) : (
+        <div>
+          {upComingEvent.length > 0 ? (
+            <div className={styles.event_section}>
+              <h1 className={styles.title}>
+                {' '}
+                Upcoming <span className={styles.red}> Events</span>
+              </h1>
+              {upComingEvent.map((data, i) => {
+                return <UpcomingEventCard data={data} key={i} />;
+              })}
+            </div>
+          ) : null}
+          {pastEvent.length > 0 ? (
+            <div className={styles.event_section}>
+              <h1 className={styles.title}>
+                Past <span className={styles.red}> Events</span>
+              </h1>
+              <div className={styles.event_content_section}>
+                {pastEvent.map((data, i) => {
+                  return <PastEventCard data={data} key={i} />;
+                })}
+              </div>
+            </div>
+          ) : null}
         </div>
-      ) : null}
-      {pastEvent.length > 0 ? (
-        <div className={styles.event_section}>
-          <h1 className={styles.title}>
-            Past <span className={styles.red}> Events</span>
-          </h1>
-          <div className={styles.event_content_section}>
-            {pastEvent.map((data, i) => {
-              return <PastEventCard data={data} key={i} />;
-            })}
-          </div>
-        </div>
-      ) : null}
+      )}
     </>
   );
 };
